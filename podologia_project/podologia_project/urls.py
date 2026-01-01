9ae937174fb74e195be31b23dfc6e30c45057091
eden <eden.sfr.fr@gmail.com>
2026-01-01 20:48:04 -0300

    Mejorar diseño detalle paciente y galeria

--- podologia_project/podologia_project/urls.py
+++ podologia_project/podologia_project/urls.py
@@ -2,15 +2,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views # Importamos vistas de auth
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Sistema de autenticación (login/logout)
+    
+    # --- BLOQUEO DE LOGIN ---
+    # Si alguien intenta entrar a 'login' y ya está autenticado, lo manda a la lista.
+    path('accounts/login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
+    
+    # Incluimos el resto de las URLs de autenticación (logout, password reset, etc.)
     path('accounts/', include('django.contrib.auth.urls')), 
+    
     # Rutas de la aplicación principal
     path('', include('pacientes.urls')), 
 ]
 
-# Configuración para servir archivos multimedia (fotos) en desarrollo
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
