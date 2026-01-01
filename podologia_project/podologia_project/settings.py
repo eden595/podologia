bc078bed3a90b051efc6e6bc8149a135936d3f55
eden <eden.sfr.fr@gmail.com>
2026-01-01 14:22:04 -0300

    Mejorar diseño detalle paciente y galeria

--- podologia_project/podologia_project/settings.py
+++ podologia_project/podologia_project/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '[REDACTED_DJANGO_SECRET_KEY]'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['eden2001.pythonanywhere.com', '127.0.0.1', 'localhost']
 # Application definition
