22acfafea4b8e8959778ef5b94e46bba32901961
eden <eden.sfr.fr@gmail.com>
2026-01-02 00:43:43 -0300

    Mejorar diseño detalle paciente y galeria

--- podologia_project/pacientes/models.py
+++ podologia_project/pacientes/models.py
@@ -1,35 +1,25 @@
 from django.db import models
 from django.utils import timezone
-from PIL import Image  # Librería para procesar imágenes
-from io import BytesIO # Para manejar archivos en memoria
+from PIL import Image
+from io import BytesIO
 from django.core.files.uploadedfile import InMemoryUploadedFile
 import sys
+import os # Necesario para borrar archivos
+from django.db.models.signals import post_delete # Necesario para la señal de borrado
+from django.dispatch import receiver # Necesario para recibir la señal
 
-# --- FUNCIÓN MÁGICA PARA COMPRIMIR FOTOS ---
+# --- FUNCIÓN PARA COMPRIMIR FOTOS ---
 def comprimir_imagen(image_field, quality=60):
-    """
-    Toma una imagen, la redimensiona si es muy grande y baja la calidad
-    para ahorrar espacio en el servidor.
-    """
-    # 1. Abrir la imagen
     img = Image.open(image_field)
-    
-    # 2. Convertir a RGB (necesario si la foto es PNG o tiene transparencias)
     if img.mode != 'RGB':
         img = img.convert('RGB')
-    
-    # 3. Redimensionar si es muy grande (Max 1280px de ancho)
     max_width = 1280
     if img.width > max_width:
         output_size = (max_width, int(max_width * img.height / img.width))
         img.thumbnail(output_size)
-    
-    # 4. Guardar la imagen comprimida en memoria
     output = BytesIO()
     img.save(output, format='JPEG', quality=quality, optimize=True)
     output.seek(0)
-    
-    # 5. Devolver el nuevo archivo ligero para que Django lo guarde
     return InMemoryUploadedFile(
         output, 
         'ImageField', 
@@ -66,7 +56,6 @@ class Tratamiento(models.Model):
     firma = models.TextField() 
 
     def save(self, *args, **kwargs):
-        # Si hay foto nueva, la comprimimos antes de guardar
         if self.foto and not self.foto._committed:
             self.foto = comprimir_imagen(self.foto)
         super().save(*args, **kwargs)
@@ -80,7 +69,6 @@ class FotoGaleria(models.Model):
     fecha_subida = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
-        # Comprimir imagen al guardar
         if self.imagen and not self.imagen._committed:
             self.imagen = comprimir_imagen(self.imagen)
         super().save(*args, **kwargs)
@@ -94,10 +82,35 @@ class FotoTratamiento(models.Model):
     fecha_subida = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
-        # Comprimir imagen al guardar
         if self.imagen and not self.imagen._committed:
             self.imagen = comprimir_imagen(self.imagen)
         super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Foto extra del tratamiento {self.tratamiento.id}"
+
+# ==========================================
+#        SEÑALES DE BORRADO AUTOMÁTICO
+# ==========================================
+# Esto hace que cuando borres en la web, se borre el archivo del disco
+
+@receiver(post_delete, sender=Tratamiento)
+def borrar_foto_tratamiento(sender, instance, **kwargs):
+    """ Borra la foto principal del tratamiento """
+    if instance.foto:
+        if os.path.isfile(instance.foto.path):
+            os.remove(instance.foto.path)
+
+@receiver(post_delete, sender=FotoGaleria)
+def borrar_foto_galeria(sender, instance, **kwargs):
+    """ Borra la foto de la galería """
+    if instance.imagen:
+        if os.path.isfile(instance.imagen.path):
+            os.remove(instance.imagen.path)
+
+@receiver(post_delete, sender=FotoTratamiento)
+def borrar_foto_extra(sender, instance, **kwargs):
+    """ Borra las fotos extra del tratamiento """
+    if instance.imagen:
+        if os.path.isfile(instance.imagen.path):
+            os.remove(instance.imagen.path)
